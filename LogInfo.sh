72a21ca4c9b8cbcaf9728b960abf9b652f577208
manasmallik98 <mallikmanas0@gmail.com>
2019-12-09 21:55:34 +0530

    modified LogInfo for better understanding

--- LogInfo.sh
+++ LogInfo.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
-# This script is used to check Error of a log file named LogInfo.txt file
-while read LogInfo
-do
-	grep -c "Error"
-done<LogInfo.txt
 
-while read LogInfo
-do
-	grep -n "Error"
-done<LogInfo.txt
+file="/c/Users/computer/manas/unix_tasks/LogInfo.txt"
+error_line_count=`grep -i error $file | wc -l`
+error_line_number=`grep -n Error $file`
+lines_without_error=`grep -v Error $file`
+first_five_lines=`sed '1,5!d' $file`
+last_five_lines=`sed '6,11!d' $file`
+thirdline_to_sixthline=`sed '3,6!d' $file`
+echo "$file contains $error_line_count number lines having error word in it"
+echo "in $file the error lines are $error_line_number"
+echo "the lines without error are $lines_without_error"
+echo "first 5 lines are $first_five_lines"
+echo "last 5 lines are $last_five_lines"
+echo "3re line to 6th line are $thirdline_to_sixthline"
 
-while read LogInfo
-do
-	grep -v "Error"
-done<LogInfo.txt
+
+
