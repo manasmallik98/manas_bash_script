41f29d3793e4c3072b5ea48f119ebd6218c9bef5
manasmallik98 <58525718+manasmallik98@users.noreply.github.com>
2019-12-22 22:16:09 +0530

    Update max_char.sh

--- max_char.sh
+++ max_char.sh
@@ -5,3 +5,6 @@ read Input
 output=(`echo $Input | grep -o . | sort | uniq -c | sort -nr | head -n1`)
 
 echo ${output[1]} appeared ${output[0]} times in the string $Input
+
+# let's give a string input "abbcccdddd"
+# output= d appeared 4 times in the string "abbcccdddd"
